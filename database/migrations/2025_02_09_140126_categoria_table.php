736a54e2283c738d5c3a8f980df22dd8c678920e
MaicolCorrea <maicolsmithcorrealopez@gmail.com>
2025-02-09 09:16:16 -0500

    Creación de tabla category

--- database/migrations/2025_02_09_140126_categoria_table.php
+++ database/migrations/2025_02_09_140126_categoria_table.php
@@ -6,19 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        //
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->string('category')->unique();
+            $table->text('description')->nullable();
+            $table->boolean('category_state')->default(true);
+            $table->timestamps();
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        
     }
 };
